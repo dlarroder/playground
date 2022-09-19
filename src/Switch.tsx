c3dbbd4e1981e64d7a32cfd8bdee0f06d1d87b52
Dale Larroder <dlarroder@gmail.com>
2022-09-19 23:33:02 +0800

    feat: switch variants

--- src/Switch.tsx
+++ src/Switch.tsx
@@ -1,23 +1,59 @@
-import { Switch as HeadlessSwitch } from '@headlessui/react';
+import { Switch as HSwitch } from '@headlessui/react';
+import classNames from 'classnames';
 import React from 'react';
 
 export interface SwitchProps {
   checked: boolean;
   onChange: (value: boolean) => void;
+  size?: 'small' | 'default' | 'large';
+  disabled?: boolean;
 }
 
-export const Switch = ({ checked, onChange }: SwitchProps) => (
-  <HeadlessSwitch
+export const Switch = ({
+  checked,
+  onChange,
+  size = 'default',
+  disabled,
+}: SwitchProps) => (
+  <HSwitch
     checked={checked}
     onChange={onChange}
-    className={`${checked ? 'bg-blue-500' : 'bg-gray-400'}
-          relative inline-flex flex-shrink-0 h-7 w-14 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+    className={classNames(
+      `group relative inline-flex border-2 border-transparent rounded-full 
+         cursor-pointer transition-colors ease-in-out duration-400
+         focus:ring-2 focus:ring-offset-2 focus:ring-pink-400 
+         focus:outline-none`,
+      {
+        'h-4 w-8': size === 'small',
+        'h-5 w-10': size === 'default',
+        'h-6 w-11': size === 'large',
+        'bg-pink-400': checked,
+        'bg-gray-300': !checked,
+        'opacity-70 cursor-not-allowed': disabled,
+      }
+    )}
   >
-    <span className="sr-only">Use setting</span>
+    <span className="sr-only">Switch example</span>
     <span
       aria-hidden="true"
-      className={`${checked ? 'translate-x-7' : 'translate-x-0'}
-            pointer-events-none inline-block h-6 w-6 rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
+      className={classNames(
+        `pointer-events-none inline-block rounded-full bg-white shadow-lg 
+        transform ring-0 transition ease-in-out duration-400`,
+        {
+          'h-3 w-3 translate-x-4 group-active:w-4 group-active:translate-x-3':
+            size === 'small' && checked,
+          'h-3 w-3 translate-x-0 group-active:w-4':
+            size === 'small' && !checked,
+          'h-4 w-4 translate-x-5 group-active:w-5 group-active:translate-x-4':
+            size === 'default' && checked,
+          'h-4 w-4 translate-x-0 group-active:w-5':
+            size === 'default' && !checked,
+          'h-5 w-5 translate-x-5 group-active:w-6 group-active:translate-x-4':
+            size === 'large' && checked,
+          'h-5 w-5 translate-x-0 group-active:w-6':
+            size === 'large' && !checked,
+        }
+      )}
     />
-  </HeadlessSwitch>
+  </HSwitch>
 );
--- stories/Switch.stories.tsx
+++ stories/Switch.stories.tsx
@@ -7,8 +7,29 @@ export default {
   component: Switch,
 } as ComponentMeta<typeof Switch>;
 
+export function Small() {
+  const [active, setActive] = useState(false);
+
+  return <Switch checked={active} onChange={setActive} size="small" />;
+}
+
 export function Default() {
   const [active, setActive] = useState(false);
 
   return <Switch checked={active} onChange={setActive} />;
+}
+
+export function Large() {
+  const [active, setActive] = useState(false);
+
+  return <Switch checked={active} onChange={setActive} size="large" />;
+}
+
+export function Disabled() {
+  return (
+    <div className="flex flex-col space-y-3">
+      <Switch checked onChange={() => undefined} disabled />
+      <Switch checked={false} onChange={() => undefined} disabled />
+    </div>
+  );
 }
