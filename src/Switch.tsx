edcc814c8f23966bbcf9ffab40070662fa811a11
Dale Larroder <dlarroder@gmail.com>
2022-09-24 18:24:49 +0800

    fix: switch > handle disable

--- src/Switch.tsx
+++ src/Switch.tsx
@@ -14,46 +14,56 @@ export const Switch = ({
   onChange,
   size = 'default',
   disabled,
-}: SwitchProps) => (
-  <HSwitch
-    checked={checked}
-    onChange={onChange}
-    className={classNames(
-      `group relative inline-flex border-2 border-transparent rounded-full 
-         cursor-pointer transition-colors ease-in-out duration-400
-         focus:ring-2 focus:ring-offset-2 focus:ring-pink-400 
-         focus:outline-none`,
-      {
-        'h-4 w-8': size === 'small',
-        'h-5 w-10': size === 'default',
-        'h-6 w-11': size === 'large',
-        'bg-pink-400': checked,
-        'bg-gray-300': !checked,
-        'opacity-70 cursor-not-allowed': disabled,
-      }
-    )}
-  >
-    <span className="sr-only">Switch example</span>
-    <span
-      aria-hidden="true"
+}: SwitchProps) => {
+  function handleChange(v: boolean) {
+    if (disabled) {
+      return;
+    }
+
+    onChange(v);
+  }
+
+  return (
+    <HSwitch
+      checked={checked}
+      onChange={handleChange}
       className={classNames(
-        `pointer-events-none inline-block rounded-full bg-white shadow-lg 
-        transform ring-0 transition ease-in-out duration-400`,
+        `group relative inline-flex border-2 border-transparent rounded-full 
+       cursor-pointer transition-colors ease-in-out duration-400
+       focus:ring-2 focus:ring-offset-2 focus:ring-pink-400 
+       focus:outline-none`,
         {
-          'h-3 w-3 translate-x-4 group-active:w-4 group-active:translate-x-3':
-            size === 'small' && checked,
-          'h-3 w-3 translate-x-0 group-active:w-4':
-            size === 'small' && !checked,
-          'h-4 w-4 translate-x-5 group-active:w-5 group-active:translate-x-4':
-            size === 'default' && checked,
-          'h-4 w-4 translate-x-0 group-active:w-5':
-            size === 'default' && !checked,
-          'h-5 w-5 translate-x-5 group-active:w-6 group-active:translate-x-4':
-            size === 'large' && checked,
-          'h-5 w-5 translate-x-0 group-active:w-6':
-            size === 'large' && !checked,
+          'h-4 w-8': size === 'small',
+          'h-5 w-10': size === 'default',
+          'h-6 w-11': size === 'large',
+          'bg-pink-400': checked,
+          'bg-gray-300': !checked,
+          'opacity-70 cursor-not-allowed': disabled,
         }
       )}
-    />
-  </HSwitch>
-);
+    >
+      <span className="sr-only">Switch example</span>
+      <span
+        aria-hidden="true"
+        className={classNames(
+          `pointer-events-none inline-block rounded-full bg-white shadow-lg 
+      transform ring-0 transition ease-in-out duration-400`,
+          {
+            'h-3 w-3 translate-x-4 group-active:w-4 group-active:translate-x-3':
+              size === 'small' && checked,
+            'h-3 w-3 translate-x-0 group-active:w-4':
+              size === 'small' && !checked,
+            'h-4 w-4 translate-x-5 group-active:w-5 group-active:translate-x-4':
+              size === 'default' && checked,
+            'h-4 w-4 translate-x-0 group-active:w-5':
+              size === 'default' && !checked,
+            'h-5 w-5 translate-x-5 group-active:w-6 group-active:translate-x-4':
+              size === 'large' && checked,
+            'h-5 w-5 translate-x-0 group-active:w-6':
+              size === 'large' && !checked,
+          }
+        )}
+      />
+    </HSwitch>
+  );
+};
