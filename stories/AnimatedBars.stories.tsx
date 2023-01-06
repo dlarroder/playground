eae670aa73c7b9b29d123788c93d60c2322dfa92
Dale Larroder <dlarroder@gmail.com>
2023-01-06 21:36:26 +0800

    fix: animation stories

--- stories/AnimatedBars.stories.tsx
+++ stories/AnimatedBars.stories.tsx
@@ -1,17 +1,20 @@
-import { ComponentMeta } from '@storybook/react';
 import React from 'react';
 import { AnimatedBars } from '../src/AnimatedBars';
 import { HoverableText } from '../src/HoverableText';
 
 export default {
   title: 'Animations',
-  component: AnimatedBars,
-} as ComponentMeta<typeof AnimatedBars>;
+  component: null,
+};
 
-export function NowPlaying() {
+export function AnimatedBarsExample() {
   return <AnimatedBars />;
 }
 
-export function FancyText() {
+AnimatedBarsExample.storyName = 'Animated Bars';
+
+export function HoverableTextExample() {
   return <HoverableText text="Absolutely" />;
 }
+
+HoverableTextExample.storyName = 'Hoverable Text';
